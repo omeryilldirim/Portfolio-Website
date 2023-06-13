6b0940051373a6399489750b92776dff0e0fd824
omeryilldirim <omeryilldirim@outlook.com>
2023-06-13 16:26:05 +0300

    home revised

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
-
 import "./Home.scss";
 
 const Home = () => {
   return (
     <div className="home-container">
-      <h3 className="greet"> </h3>
+      <img
+        className="object_developer"
+        src="https://www.svgrepo.com/show/308016/programmer-software-engineer-coder-software-developer.svg"
+        width="125px"
+        alt="developer"
+      />
       <h1 className="name">Ömer YILDIRIM</h1>
       <h3 className="title">Full-Stack Web Developer</h3>
     </div>
