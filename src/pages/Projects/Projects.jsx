956050335810e1209564dfcf0f75fcc2ab89830a
omeryilldirim <omeryilldirim@outlook.com>
2023-06-21 11:05:14 +0300

    portfolio updated

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,5 +1,6 @@
-import "./Projects.scss"
-import {imageData} from "../../helper/imageData.js"
+import "./Projects.scss";
+import { imageData } from "../../helper/imageData.js";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
   return (
@@ -8,14 +9,20 @@ const Projects = () => {
         <h1 className="title">My Projects</h1>
       </div>
       <div className="projects-items">
-        {imageData.map((item,i) => (
-                  <div key={i+1} className="projects-item">
-                      <img className="project-image" src={item.image} alt="My Project" />
-                  </div>
+        {imageData.map((item, i) => (
+          <Link key={i + 1} to={item.link} target="_blank" className="project-navigator">
+            <div className="projects-item">
+              <img
+                className="project-image"
+                src={item.image}
+                alt="My Project"
+              />
+            </div>
+          </Link>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
