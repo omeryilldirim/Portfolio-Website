e3b3fb59dbb128a198a86cea99b0311f868546e6
Ömer Yıldırım <omeryilldirim@outlook.com>
2024-02-06 17:51:30 +0300

    agentsphere app

--- src/helper/imageData.js
+++ src/helper/imageData.js
@@ -22,9 +22,11 @@ import tour from "../assets/img/tour.png";
 import weather from "../assets/img/weather.jpg";
 import webdesign from "../assets/img/webdesign.gif";
 import chakra from "../assets/img/chakra.gif";
+import agentsphereapp from "../assets/img/agentsphereapp.gif";
 
 export const imageData = [
-  { image: stock, link:"https://mryldrm-stock.netlify.app/" },
+  { image: agentsphereapp, link:"" },
+  { image: stock, link:"https://apps.apple.com/tr/app/agentsphere/id6472581045?platform=iphone" },
   { image: blog, link:"https://mryldrm-blog-app.netlify.app/" },
   { image: movie, link:"https://mryldrm-movie.netlify.app/" },
   { image: recipe, link:"https://mryldrm-recipe.netlify.app/" },
